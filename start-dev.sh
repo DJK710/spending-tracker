025a6208fbbb8e637a47d54a7404c570a8663c2f
DJK710 <djvanderkuijl@gmail.com>
2026-06-30 22:13:41 +0200

    Fix local development

--- start-dev.sh
+++ start-dev.sh
@@ -6,7 +6,7 @@ sudo service postgresql start
 echo "Starting backend..."
 cd backend
 source ./venv/bin/activate
-uvicorn app.main:app --reload &
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 &
 BACKEND_PID=$!
 
 echo "Starting frontend..."
